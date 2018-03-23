7445be20a8ba8a66bd66e0ecaed291967eac1e47
Punyam <punyam@Punyams-MacBook-Pro.local>
2018-03-23 16:26:28 +0530

    web3 integration attempt

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Web3 } from 'web3-core';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  web3 :any;
+
   constructor() { }
 
   ngOnInit() {
+    this.getBalance();
   }
 
+  getBalance() {
+    console.log("inside getBalance");
+  this.web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/rDtDtyNmAVjB12zhj5nn"));
+    
+    if (typeof this.web3 === 'undefined') {
+      console.log('No web3 found, get MetaMask!')
+    } else {
+      console.log('Web3 found! Tips welcome!')
+    }
+
+    let account = this.web3.eth.accounts[0];
+
+    this.web3.eth.getBalance(account, (err, balance) => {
+      if (err) {
+        console.log(err.message)
+      }
+      
+      const ether = this.web3.fromWei(balance, 'ether')
+      console.log(`Account balance: ${ether.toString()}`);
+    })
+  }
 }
