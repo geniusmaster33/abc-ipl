a830578031bd38277b49f4a1a1602a0ef9e68e31
nitesh singh <nitrek@users.noreply.github.com>
2018-04-03 20:34:46 +0530

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,10 +1,13 @@
-module.exports = {
-    networks: {
-         development: {
-              host: "https://kovan.infura.io/rDtDtyNmAVjB12zhj5nn",
-              port: 8545,
-              network_id: "*" // Match any network id
-            }
-       }
-};
 
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic =  "off find version cram note manage runway execute prevent auction keep suggest"
+module.exports = {
+  networks: {
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/rDtDtyNmAVjB12zhj5nn",2)
+      },
+      network_id: 42
+    }   
+  }
+};
