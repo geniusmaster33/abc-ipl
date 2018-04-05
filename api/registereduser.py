6e177fd3173ef48f631a12a6e1cc9af568b3e042
nitrek <koolestnitesh@gmail.com>
2018-04-05 14:00:33 +0000

    added api code

---
+++ api/registereduser.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask_cors import CORS
+import pickle as cPickle
+import os
+import logging
+import json
+logger = logging.getLogger('werkzeug')
+handler = logging.FileHandler('access.log')
+logger.addHandler(handler)
+app = Flask(__name__)
+app.logger.addHandler(handler)
+app.config.update(DEBUG = False)
+CORS(app)
+@app.route("/getusers")
+def checkuser():
+#     username = request.args.get('username')
+#        if(username in usernameFullname.keys()):
+	if(os.path.isfile("usernameFullname.pickle")):
+        	with open(r"usernameFullname.pickle", "r") as input_file:
+                	usernameFullname= cPickle.load(input_file)
+		        return json.dumps(usernameFullname)
+        #else:
+        return "NA"
+@app.route("/getuserspk")
+def checkpk():
+#     username = request.args.get('username')
+#        if(username in usernameFullname.keys()):
+        if(os.path.isfile("usernamePK.pickle")):
+                with open(r"usernamePK.pickle", "r") as input_file:
+                        usernameFullname= cPickle.load(input_file)
+                        return json.dumps(usernameFullname)
+        #else:
+        return "NA"
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0",port=4030,debug=False,threaded=True)
---
+++ api/time.py
@@ -0,0 +1,21 @@
+from flask import Flask,jsonify
+import pytz
+from datetime import datetime
+from flask_cors import CORS
+import logging
+app = Flask(__name__)
+CORS(app)
+logger = logging.getLogger('werkzeug')
+handler = logging.FileHandler('timeaccess.log')
+logger.addHandler(handler)
+app.logger.addHandler(handler)
+
+@app.route('/getTime')
+def hello_name():
+	naive_dt = datetime.now()
+	tz = pytz.timezone('Europe/London')
+	epochtime = datetime.now(tz)
+	return epochtime.strftime('%s')
+
+if __name__ == '__main__':
+   app.run(debug = False,host ='0.0.0.0',threaded=True)
