153a875e54b6f4985068fd38493cfbc9849cfcc4
Punyam <punyam@Punyams-MacBook-Pro.local>
2018-04-06 19:34:24 +0530

    Added new components

--- src/app/component/leader-board/leader-board.component.ts
+++ src/app/component/leader-board/leader-board.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as eip20_artifact from '../../../../build/contracts/EIP20.json';
+import * as ipl_artifact from '../../../../build/contracts/Ipl.json';
+import * as registration_artifact from '../../../../build/contracts/Registration.json';
+import { Web3Service } from '../../util/web3.service';
 
 @Component({
   selector: 'app-leader-board',
@@ -9,12 +13,17 @@ export class LeaderBoardComponent implements OnInit {
 
   leadersList;
   aiLeadersList;
+  Regn: any;
 
-  constructor() { }
+  names;
+  balances;
+
+  constructor(private web3Service: Web3Service) { }
 
   ngOnInit() {
     this.leadersList = this.getLeaderBoard();
     this.aiLeadersList = this.getAILeaders();
+    this.getLeaderBoardData();
   }
 
   getLeaderBoard() {
@@ -63,4 +72,25 @@ export class LeaderBoardComponent implements OnInit {
     return this.leadersList.filter((leader) => (leader.category === 'machine'));
   }
 
+  getLeaderBoardData() {
+    this.web3Service.artifactsToContract(registration_artifact)
+      .then((response) => {
+        this.Regn = response;
+        this.Regn.deployed().then((instance) => {
+          console.log("Regn contract ------- ", instance);
+          instance.getLeaderBoard.call().then((v) => {
+            console.log("LeaderBoard Details", v);
+            this.leadersList = v;
+            this.names = this.leadersList[0];
+            this.balances = this.leadersList[1];
+
+            console.log(this.names);
+
+
+            
+          });
+        },
+          (e) => { });
+      })
+  }
 }
