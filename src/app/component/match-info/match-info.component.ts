48759ec8290521834be5948bacb8788f95500f02
punyam <punyam.patnaik@gmail.com>
2018-05-21 17:57:06 +0530

    Playoff matches added

--- src/app/component/match-info/match-info.component.ts
+++ src/app/component/match-info/match-info.component.ts
@@ -44,7 +44,7 @@ export class MatchInfoComponent implements OnInit {
   }
 
   fetchMatches(currentDate) {
-    const url = './../assets/info/match1.json';
+    const url = './../assets/info/match2.json';
 
     this.http.get(url).subscribe(
       (data) => {
--- src/app/component/recent-predictions/recent-predictions.component.ts
+++ src/app/component/recent-predictions/recent-predictions.component.ts
@@ -128,7 +128,7 @@ export class RecentPredictionsComponent implements OnInit {
   }
 
   loadMatchList(currentDate) {
-    const url = './../assets/info/match1.json';
+    const url = './../assets/info/match2.json';
     //currentDate = moment.now();
 
     this.http.get(url).subscribe(
