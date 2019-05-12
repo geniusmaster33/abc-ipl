353bdace354ae7b8f22517437c28121d0dcf4bfe
punyam <punyam.patnaik@gmail.com>
2019-05-12 09:33:16 +0530

    Final match added

--- src/app/component/recent-predictions/recent-predictions.component.ts
+++ src/app/component/recent-predictions/recent-predictions.component.ts
@@ -128,7 +128,7 @@ export class RecentPredictionsComponent implements OnInit {
   }
 
   loadMatchList(currentDate) {
-    const url = './../assets/info/match14.json';
+    const url = './../assets/info/match15.json';
     //currentDate = moment.now();
 
     this.http.get(url).subscribe(
